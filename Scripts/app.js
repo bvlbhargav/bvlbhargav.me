a5ea174bb933a8498e2e95d894f45847414b2878
lakshmibhargavab <lakshmibhargavab@proarch.com>
2017-05-17 18:04:54 +0530

    included react component script file

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,5 @@
 // Primary placehoolder for the scripts for bvlbhargav.me project
+
+//Rendering HellowWorld component on page
+
+ReactDOM.Render(<helloWorld/>,document.getElementById('content'));
---
+++ Scripts/reactComponents.js
@@ -0,0 +1,10 @@
+
+// Hello World component
+
+var helloWorld = React.createClass(
+    {
+        render:function(){
+            return (<div>Hello Lashmi Bhargava</div>);
+        }
+    }
+)
