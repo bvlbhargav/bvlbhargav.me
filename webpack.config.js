3cc1078b2130dfcf7d8cf4a37811bf326b7825ef
lakshmibhargavab <lakshmibhargavab@proarch.com>
2017-05-17 21:37:20 +0530

    babel setup for build and run

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+var webpack = require('webpack');
 var path = require('path');
 
+//var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, '/');
+
+
 module.exports = {
-  entry: './Scripts/app.js',
+  entry: './Scripts/reactComponents.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+    module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
   }
 };
