e9ab758335a1f7e1171aa6ffb5022c80d79bd679
lakshmibhargavab <lakshmibhargavab@proarch.com>
2017-05-18 15:32:31 +0530

    added like component

--- Scripts/reactComponents.jsx
+++ Scripts/reactComponents.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+
+//Component import from other JSX files
+import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
-  render () {
-    return <p> Hello lakshmi Bhargava</p>;
-  }
+    render() {
+        return (
+            <div>
+                <p> Hello lakshmi Bhargava</p>
+                <AwesomeComponent />
+            </div>
+        );
+    }
 }
 
-render(<App/>, document.getElementById('content'));
+render(<App />, document.getElementById('content'));
